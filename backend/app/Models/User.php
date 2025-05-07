09a1d21a292dab6b49e5417365eb769de2b6604f
elhyane <businessothman1@gmail.com>
2025-05-07 13:29:52 +0200

    minor changes in backend

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -18,7 +18,11 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'cin',
+        'city',
+        'phone',
         'email',
         'password',
     ];
