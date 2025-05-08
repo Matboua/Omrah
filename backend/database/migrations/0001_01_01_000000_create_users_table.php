0c1e32af80f93a8b237c00746053be055a6fc034
elhyane <businessothman1@gmail.com>
2025-05-08 17:59:34 +0200

    added isadmin middleware and role migration

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('city');
             $table->string('phone')->unique();            
             $table->string('email')->unique();
+            $table->enum('role', ['admin', 'client'])->default('client');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
